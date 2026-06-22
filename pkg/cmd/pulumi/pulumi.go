5649b984dd8037a1c06905cbf1404939ab0f6fff
Ian Wahbe <me@iwahbe.com>
2026-06-22 14:49:28 +0200

    package info

--- pkg/cmd/pulumi/pulumi.go
+++ pkg/cmd/pulumi/pulumi.go
@@ -472,6 +472,12 @@ func NewPulumiCmd() (*cobra.Command, func()) {
 	cmd.PersistentFlags().StringVar(
 		&color, "color", "auto", "Colorize output. Choices are: always, never, raw, auto")
 
+	nCtx := needle.Context{
+		WS:  pkgWorkspace.Instance,
+		Env: env.Global(),
+		LM:  cmdBackend.DefaultLoginManager,
+	}
+
 	setCommandGroups(cmd, []commandGroup{
 		// Common commands:
 		{
@@ -507,7 +513,7 @@ func NewPulumiCmd() (*cobra.Command, func()) {
 			Commands: []*cobra.Command{
 				auth.NewLoginCmd(pkgWorkspace.Instance, cmdBackend.DefaultLoginManager, env.Global()),
 				auth.NewLogoutCmd(pkgWorkspace.Instance),
-				whoami.NewWhoAmICmd(needle.Context{WS: pkgWorkspace.Instance}),
+				whoami.NewWhoAmICmd(nCtx),
 				org.NewOrgCmd(),
 				project.NewProjectCmd(),
 				deployment.NewDeploymentCmd(pkgWorkspace.Instance),
@@ -527,7 +533,7 @@ func NewPulumiCmd() (*cobra.Command, func()) {
 			Commands: []*cobra.Command{
 				plugin.NewPluginCmd(),
 				schema.NewSchemaCmd(),
-				packagecmd.NewPackageCmd(needle.Context{WS: pkgWorkspace.Instance}),
+				packagecmd.NewPackageCmd(nCtx),
 			},
 		},
 		{
